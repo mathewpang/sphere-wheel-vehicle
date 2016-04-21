e23930a09021937071416c362fde1872d56cdb5c
Natsumi Otaguro <natsumi.otaguro@gmail.com>
2016-04-21 02:23:37 -0700

    Add skeleton for controller and some ugly images for GUII

---
+++ computer/unity/sphere-wheel-vehicle-gui/Assets/HTTPSender.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HTTPSender : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    // Start HTTP Request whatever initializaing we need
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    // When there is an update
+	}
+
+    void sendPost(int radius, int angle)
+    {
+        //Send a POST request to server
+    }
+}
